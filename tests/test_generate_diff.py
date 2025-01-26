3826f9cf2391c349563702b98ed982496ca1af61
Andriy Vasylenko <andrewvasilen@gmail.com>
2025-01-26 14:20:59 +0200

    add test fixture and CI

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,13 +1,25 @@
+import os
+import json
+
 from gendiff import generate_diff
 
 
-def test_equvivalent():
-    correct = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
-    assert generate_diff("file1.json", "file2.json") == correct
+def get_case_file(filename):
+    f = open(os.path.abspath("tests/test_data/" + filename), "r")
+    return f.read()
+
+
+def test_both_empty():
+    assert generate_diff(
+        "tests/test_data/empty.json", "tests/test_data/empty.json"
+    ) == get_case_file("empty.txt")
+
+
+def test_exercise():
+    assert generate_diff(
+        "tests/test_data/file1.json", "tests/test_data/file2.json"
+    ) == get_case_file("exercise_case.txt")
+
+
+# for filename in cases.keys():
+# os.remove(filename + ".json")
